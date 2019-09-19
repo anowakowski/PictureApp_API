d032db50af0c20f9a7cc03d95fa9d45219fd1a57
mvein <ekonto@marcinzyla.com>
2019-09-19 10:45:40 +0200

    Missing unit tests has been added - part I

---
+++ PictureApp.API.Tests/Messaging/PhotoUploadedNotificationHandlerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PictureApp.Messaging;
+
+namespace PictureApp.API.Tests.Messaging
+{
+    [TestFixture]
+    public class PhotoUploadedNotificationHandlerTests : GuardClauseAssertionTests<PhotoUploadedNotificationHandler>
+    {
+        [Test]
+        public async Task Handle_WhenCalled_AttemptToUpdatePhotoForUserAndRemoveItFromFilesStorageExpected()
+        {
+            // ARRANGE            
+            // ACT
+            // ASSERT
+        }
+    }
+}
